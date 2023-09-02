b9682d3c87c0ad13e72492617e212dda2f75fe2d
shamilgaleyev <galeyevshamil@gmail.com>
2023-09-01 22:16:31 -0400

    commit2

---
+++ src/Package/Class1.java
@@ -0,0 +1,10 @@
+package Package;
+
+public class Class1 {
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("I am blessed");
+            Thread.sleep(2000);
+        }
+    }
+}
